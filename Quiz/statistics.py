bf81f0369b2265adfb3f811a14bd2d6c8eeea08c
Alisher Zakir <a3zakir@gmail.com>
2020-03-05 23:46:06 +0000

    hello there

--- Quiz/statistics.py
+++ Quiz/statistics.py
@@ -1,3 +1,56 @@
 from data.connection import Connection
 
 # def save_stats()
+
+class Statistics():
+    def __init__(self):
+        self.ensure_table_exists()
+
+    def ensure_table_exists():
+        with Connection() as con:
+            con.execute('''
+            CREATE TABLE IF NOT EXISTS statistics(
+            id INTEGER PRIMARY KEY,
+            question_id INTEGER,
+            corrects INTEGER,
+            incorrects INTEGER,
+            skips INTEGER,
+            time INTEGER,
+            FOREIGN KEY(question_id) REFERENCES questions(id)
+            )''')
+
+    def create_stats(id):
+        with Connection() as con:
+            with con:
+                con.execute('''
+                INSERT INTO statistics
+                (question_id, corrects, incorrects, skips, time)
+                VALUES
+                (?, 0, 0, 0, 0)
+                ''', (id, ))
+
+    def get_stats(id):
+        with Connection() as con:
+            with con:
+                return con.execute('''
+                SELECT *
+                FROM statistics
+                WHERE question_id = ?
+                ''', str(id)).fetchone()
+
+    def update_stats(obj):
+        obj = {k: str(v) for k, v in obj.items()}
+        with Connection() as con:
+            with con:
+                con.execute('''
+                UPDATE statistics
+                SET corrects = ?,
+                incorrects = ?,
+                skips = ?,
+                time = ?
+                WHERE question_id = ?
+                ''', (obj["corrects"],
+                      obj["incorrects"],
+                      obj["skips"],
+                      obj["time"],
+                      obj["id"]))
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-from Quiz.multiplechoice import get_question, save_question, Multiplechoice
+from Quiz.statistics import Statistics
 
-print(get_question(2))
-Multiplechoice.add_question({"text": "A", "correct": "A", "incorrect": ["D", "B", "C"]})
-print(get_question(2))
+
+Statistics.update_stats({"id": 4,
+                         "corrects": 3,
+                         "skips": 5,
+                         "incorrects": 3,
+                         "time": 33})
+id, q_id, corrects, incorrects, skips, time =  Statistics.get_stats(4)
+
+print(skips)

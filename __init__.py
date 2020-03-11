53d97c6a439f78b8654f83b3f56c8210387cbd73
Alisher Zakir <a3zakir@gmail.com>
2020-03-11 19:25:22 +0000

    fixing folder hierarchy

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# include pages that you would like to import from this folder
-
-from .multiple_choice import MultipleChoice, EndScreen
-from .settings import Settings
-from .welcome import Welcome

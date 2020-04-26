f8d09ce2e76d36306c4c0722c606f9e5fd1613ad
Alisher Zakir <a3zakir@gmail.com>
2020-04-26 15:41:44 +0100

    update timer for hangman

--- UI/hangmanUI.py
+++ UI/hangmanUI.py
@@ -6,6 +6,7 @@ from Quiz.hangman import Hangman as HangmanDB
 from UI.page import Page
 from UI.usefulLabel import GridLabel
 from UI.hoverButton import HoverButton
+from UI.components.timerlabel import TimerLabel
 class Hangman(Page):
         # root.tag_raise(canvas)
     def create(self):
@@ -13,7 +14,7 @@ class Hangman(Page):
         self.question = GridLabel(self, text = "Question 1: ", pos=(1, 5))
         self.question_label = GridLabel(self, text = "What does the fox say? ", pos=(2, 5))
         GridLabel(self, text = "Time Elapsed ", pos=(2,6))
-        self.time = GridLabel(self, textvariable=self.mainUI.timer, pos=(2, 7))  #takes a string variable created in main UI, it tracks the timer.
+        self.time = TimerLabel(self, mainUI=self.mainUI, pos=(2, 7))  #takes a string variable created in main UI, it tracks the timer.
 
         self.correctUnderscore = StringVar()
         self.correctLetters = GridLabel(self, textvariable=self.correctUnderscore, pos=(4, 7), cspan=8) #Because the variable constantly changes, Label updates accordingly.
@@ -37,7 +38,6 @@ class Hangman(Page):
         #THIS creates a place for the image, but it is empty, when refresh is run, we implement the initial image.
         self.canvas = GridLabel(self, pos=(4, 1), rspan=11, cspan=5)
 
-
     def next_q(self):
         self.questionID, self.question_label["text"], _, self.correctAnswer = next(self.questions) #create variables, which asks Database for next question, the database is going to provide
         #data as a tuple, and we unpack it into the empty variables. ",_," means like an empty variable, which dont want to use it.
@@ -45,12 +45,9 @@ class Hangman(Page):
         self.lives = 6
         self.mainUI.clock1 = time.time() # reset the clock provided from Main UI
 
-
         ################# CORRECT LETTERS #########################
         self.underscores = ["_"] * len(self.correctAnswer)
         self.correctUnderscore.set(self.underscores) #each time update the uderscore variable, we need to do .set()
-
-
 
         self.incorrect_letter_space = []
         for i in range(6):
@@ -58,7 +55,6 @@ class Hangman(Page):
             incorrect.set("_")
             GridLabel(self, textvariable=incorrect, pos=(4, 18+i))
             self.incorrect_letter_space.append(incorrect)
-
 
         ###IMAGE ####
         image = Image.open(f"./src/image{6-self.lives}.png")
@@ -73,7 +69,7 @@ class Hangman(Page):
         self.skip_button.grid(columnspan=2) # fixes layout issue
         self.end_quiz_button.show()
         self.restart_button.show()
-        self.time["textvariable"] = self.mainUI.timer #Unfreezes the timer
+        self.time.start() #Unfreezes the timer
 
         for button in self.buttons: button.configure(state=NORMAL) #UNFREEZE BUTTONS
 
@@ -81,24 +77,6 @@ class Hangman(Page):
         super().show()
         self.questions = HangmanDB.get_hangman_qs(True)  # randomly retrieve Hangman QUestions from the backend
         self.next_q()
-
-        ###IMAGE ####
-        image = Image.open(f"./src/image{6-self.lives}.png")
-        image = image.resize((200, 200))
-        imagetk = ImageTk.PhotoImage(image)
-        self.canvas["image"] = imagetk # this actually puts image into the space.
-
-        self.canvas.image = imagetk #This solves the problem of disappearing image, saves a refrence to the actual image.
-
-        #refresh the skip button back to its original text value "Skip button" not "next question button"
-        self.skip_button["text"] = "Skip"
-        self.skip_button.grid(columnspan=2) # fixes layout issue
-        self.end_quiz_button.grid()
-        self.restart_button.grid()
-        self.time["textvariable"] = self.mainUI.timer #Unfreezes the timer
-
-        for button in self.buttons: button.configure(state=NORMAL) #UNFREEZE BUTTONS
-
 
     def click_letter(self, i):
         guessed_letter = self.buttons[i]["text"].lower()
@@ -109,7 +87,6 @@ class Hangman(Page):
                 if letter.lower() == guessed_letter:
                     self.underscores[index] = letter.upper() #match index positions, assign our letter from the correct answer in the correct letters field.
             self.correctUnderscore.set(self.underscores) # after done looping, update the label all at once.
-
         else:
 
             #List comprehention: #checks if the letter in incorrect space is an underscore, if yes, then the incorrectly chosen letter is popullated in the space.
@@ -127,11 +104,11 @@ class Hangman(Page):
                 for button in self.buttons:
                     button.configure(state=DISABLED)
 
-                self.skip_button["text"] = "Next Question"
+                self.skip_button.configure(text="Next Question")
                 self.skip_button.grid(columnspan=8) # fixes layout issue
                 self.end_quiz_button.hide()
                 self.restart_button.hide()
-                self.time["textvariable"] = self.mainUI.timer.get()
+                self.time.pause()
 
 
 
